6c380311ecd1f52822798cb406f337ca09e49ba2
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2024-10-01 00:46:44 +0700

    feat(payment):CRUD success, fail, retrieve

---
+++ src/app/modules/payments/payments.controller.ts
@@ -0,0 +1,58 @@
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+
+import { paymentServices } from './payments.service';
+
+const SSLPaymentHandler = catchAsync(async (req, res) => {
+  const paymentData = {
+  ...req.body, 
+  userId: req.user.userId
+  }
+  await paymentServices.SSLPayment( paymentData,res);
+});
+
+const paymentSuccessHandler = catchAsync(async (req, res) => {
+
+  
+  
+  const transectionId = req.params.transactionID;
+  const userId = req.user.userId;
+
+  const result = await paymentServices.paymentSuccess(userId,transectionId, res);
+  if (Object.keys(result).length > 0) {
+    res.send({success:true})
+  }
+  
+});
+
+const paymentFailHandler = catchAsync(async (req, res) => {
+
+  const transectionId = req.params.transactionID;
+  const userId = req.user.userId;
+  await paymentServices.paymentFail(userId,transectionId, res);
+  
+});
+
+
+
+const getAllPayments = catchAsync(async (req, res) => {
+  const result = await paymentServices.getAllPayments(req.query);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'All payments are retrieved successfully',
+    meta: result.meta, 
+    data: result.data,
+  });
+});
+
+
+
+export const paymentControllers = {
+  SSLPaymentHandler,
+  paymentSuccessHandler,
+  paymentFailHandler,
+  getAllPayments
+};
---
+++ src/app/modules/payments/payments.utils.ts
@@ -0,0 +1,54 @@
+import { Types } from 'mongoose';
+import SSLCommerzPayment from 'sslcommerz-lts';
+import config from '../../config';
+
+export const SSLPaymentGateway = async (price: number, userId:string) => {
+  
+  const unique_tran_id = new Types.ObjectId().toString();
+
+  const data = {
+    total_amount: price,
+    currency: 'BDT',
+    tran_id: unique_tran_id, // use unique tran_id for each api call
+    success_url: `${config.server_url}/payment/success/${unique_tran_id}/${userId}`,
+    fail_url: `${config.server_url}/payment/fail/${unique_tran_id}/${userId}`,
+    cancel_url: 'http://localhost:3030/cancel',
+    ipn_url: 'http://localhost:3030/ipn',
+    shipping_method: 'Courier',
+    product_name: 'Computer.',
+    product_category: 'Electronic',
+    product_profile: 'general',
+    cus_name: 'Customer Name',
+    cus_email: 'customer@example.com',
+    cus_add1: 'Dhaka',
+    cus_add2: 'Dhaka',
+    cus_city: 'Dhaka',
+    cus_state: 'Dhaka',
+    cus_postcode: '1000',
+    cus_country: 'Bangladesh',
+    cus_phone: '01711111111',
+    cus_fax: '01711111111',
+    ship_name: 'Customer Name',
+    ship_add1: 'Dhaka',
+    ship_add2: 'Dhaka',
+    ship_city: 'Dhaka',
+    ship_state: 'Dhaka',
+    ship_postcode: 1000,
+    ship_country: 'Bangladesh',
+  };
+
+  const sslcz = new SSLCommerzPayment(
+    config.store_id,
+    config.store_passwd,
+    config.is_live === "true",
+  );
+
+
+  const url = await sslcz.init(data).then((apiResponse:any) => {
+    // Redirect the user to payment gateway
+    const GatewayPageURL = apiResponse.GatewayPageURL;
+    return GatewayPageURL;
+  });
+
+  return { url, tranId: unique_tran_id };
+};
